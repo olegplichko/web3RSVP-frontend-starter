e8267289f5b2907f8ff85872c2a78f2e5bcf3689
Oleg Plichko <oleg.plichko.developer@gmail.com>
2022-08-23 22:11:59 +0300

    fix typo

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { ethers } from "ethers";
 import { ConnectButton } from "@rainbow-me/rainbowkit"
-import { userAccount } from "wagmi";
+import { useAccount } from "wagmi";
 import connectContract from "../../utils/connectContract";
 import Alert from "../../components/Alert";
 
@@ -109,7 +109,40 @@ function Event({ event }) {
             <p>{event.description}</p>
           </div>
           <div className="max-w-xs w-full flex flex-col gap-4 mb-6 lg:mb-0">
-           
+            {event.eventTimestamp > currentTimestamp ? (
+              account ? (
+                checkIfAlreadyRSVPed() ? (
+                  <>
+                    <span className="w-full text-center px-6 py-3 text-base font-medium rounded-full text-real-800 bg-teal-100">
+                      You have RSVPed! 🙌
+                    </span>
+                    <div className="flex item-center">
+                      <LinkIcon className="w-6 mr-2 text-indigo-800" />
+                      <a
+                        className="text-indigo-800 truncate hover:underline"
+                        href={event.link}
+                      >
+                        {event.link}
+                      </a>
+                    </div>
+                  </>
+                ) : (
+                  <button
+                    type="button"
+                    className="w-full items-center px-6 py-3 border-transparent text-base front-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={newRSVP}
+                  >
+                    RSVP for {ethers.utils.formatEther(event.deposit)} MATIC
+                  </button>
+                )
+              ) : (
+                <ConnectButton />
+              )
+            ) : (
+              <span className="w-full text-center px-6 py-3 text-base font-medium rounded-full border-2 border-gray-200">
+                Event has ended
+              </span>
+            )}
             <div className="flex item-center">
               <UsersIcon className="w-6 mr-2" />
               <span className="truncate">
